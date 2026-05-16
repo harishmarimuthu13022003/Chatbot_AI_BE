a574fe60457fa4ad577aea26500c3e392d983e70
harishmarimuthu13022003 <harishmarimuthu1234@gmail.com>
2026-05-16 18:43:11 +0530

    update fe live url

--- main.py
+++ main.py
@@ -14,7 +14,13 @@ app = FastAPI(title="AI Chat Application", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://chatbot-ai-fe.vercel.app",
+        "chatbot-ai-fe.vercel.app",
+        "https://chatbot-ai-fe-git-main-harishmarimuthu13022003s-projects.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
